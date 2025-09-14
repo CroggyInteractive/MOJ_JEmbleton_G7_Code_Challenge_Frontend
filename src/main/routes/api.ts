7bdf4c9d3201eaa9beaa63f3b98b58b3f6537305
JohnnyEmbleton <john.embleton@justice.gov.uk>
2025-09-14 20:17:02 +0100

    Fixed loop bug

--- src/main/routes/api.ts
+++ src/main/routes/api.ts
@@ -3,21 +3,6 @@ import axios from 'axios';
 
 export default (app: Application) => {
 
-
-        // app.get('/api/create-case', async (req, res) => {
-        //     try {
-        //         // An example of connecting to the backend (a starting point)
-        //         const response = await axios.get('http://localhost:4000/get-example-case');
-        //         console.log(response.data);
-        //         res.render('home', { "example": response.data });
-        //     } catch (error) {
-        //         console.error('Error making request:', error);
-        //         res.render('home', {});
-        //     }
-        // });
-
-
-
 app.post('/api/create-case', async (req, res) => {
     const title = "hello"
     const caseNumber = req.body.caseNumber;
@@ -27,9 +12,6 @@ app.post('/api/create-case', async (req, res) => {
     const day = req.body['dueDate-day'];
     const month = req.body['dueDate-month'];
     const year = req.body['dueDate-year'];
-
-    // const formattedDay = day.padStart(2, '0');
-    // const formattedMonth = month.padStart(2, '0');
 
     const dueDate = `${year}-${month}-${day}T00:00:00`;
     console.log("Setting case details with data:", `${title} - ${caseNumber} - ${description} - ${status} - ${dueDate}`);
